e5ada179c1a14cc9259f8bca3f05474219813d8f
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-02-23 14:13:14 +0100

    Add unit tests

--- tests/testthat/test-geoparser.R
+++ tests/testthat/test-geoparser.R
@@ -9,3 +9,61 @@ test_that("Check for error in geoparser() -------", {
   expect_error(geoparser(as.factor(iris$Species)), "Argument 'x' must be a character")
   expect_error(geoparser(iris$Sepal.Length),       "Argument 'x' must be a character")
 })
+
+
+test_that("Check for geoparser() output -------", {
+  
+  test_data <- c(
+    "
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt US labore et dolore magna aliqua. USA enim ad minim 
+    veniam, quis nostrud exercitation ullamco laboris United States
+    "
+  )
+  
+  res <- geoparser(test_data)
+  
+  expect_equal(class(res), "data.frame")
+  expect_equal(nrow(res), 1L)
+  expect_equal(ncol(res), 4L)
+  
+  expect_equal(res[1, 1], "United States")
+  expect_equal(res[1, 2], 1L)
+  expect_equal(res[1, 3], 1L)
+  expect_equal(res[1, 4], 3L)
+  
+  test_data <- c(
+    "
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt US labore et dolore magna aliqua. USA enim ad minim 
+    veniam, quis nostrud exercitation ullamco laboris United States
+    ",
+    "
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt US labore et dolore magna aliqua. USA enim ad minim 
+    veniam, quis nostrud exercitation ullamco laboris Canada.
+    "
+  )
+  
+  res <- geoparser(test_data)
+  
+  expect_equal(class(res), "data.frame")
+  expect_equal(nrow(res), 3L)
+  expect_equal(ncol(res), 4L)
+  
+  expect_equal(res[1, 1], "United States")
+  expect_equal(res[2, 1], "Canada")
+  expect_equal(res[3, 1], "United States")
+  
+  expect_equal(res[1, 2], 2L)
+  expect_equal(res[1, 3], 1L)
+  expect_equal(res[1, 4], 3L)
+  
+  expect_equal(res[2, 2], 2L)
+  expect_equal(res[2, 3], 2L)
+  expect_equal(res[2, 4], 1L)
+  
+  expect_equal(res[3, 2], 2L)
+  expect_equal(res[3, 3], 2L)
+  expect_equal(res[3, 4], 2L)
+})
