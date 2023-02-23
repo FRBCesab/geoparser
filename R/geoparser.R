78a33d46edd741a1e5c42e99fc08031893e5fd0d
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-02-23 10:30:33 +0100

    feat: first version of the main function

---
+++ R/geoparser.R
@@ -0,0 +1,94 @@
+#' Detect countries names
+#'
+#' @param x a `character` in which countries will be detected. 
+#'
+#' @return a `data.frame` with: ...
+#' 
+#' @export
+#'
+#' @examples
+#' ## ----
+
+geoparser <- function(x) {
+  
+  if (missing(x)) {
+    stop("Argument 'x' is required", call. = FALSE)
+  }
+  
+  if (!is.character(x)) {
+    stop("Argument 'x' must be a character", call. = FALSE)
+  }
+  
+  
+  ## Import patterns ----
+  
+  world_countries <- world_countries[!is.na(world_countries$"parser"), ]
+  countries <- world_countries$"parser"
+
+  
+  ## Clean patterns ----
+  
+  countries <- gsub("-", " ", countries)
+  
+  
+  ## Clean content ----
+  
+  x <- strsplit(x, "\t|\n|\\s{2,}")  
+  
+  x <- lapply(x, function(.) gsub("[[:punct:]]", " ", .))
+  x <- lapply(x, function(.) gsub("\\s{1,}", " ", .))
+  x <- lapply(x, function(.) trimws(.))
+  x <- lapply(x, function(.) tolower(.))
+  x <- lapply(x, function(.) paste0(., collapse = " "))
+
+  x <- unlist(x)
+  
+  
+  ## Detect patterns ----
+  
+  detections <- lapply(x, function(page) {
+    
+    unlist(lapply(countries, function(country) {
+      
+      country <- c(
+        paste0("^",   unlist(strsplit(country, "\\|")), "$",   collapse = "|"),
+        paste0("^",   unlist(strsplit(country, "\\|")), "\\s", collapse = "|"),
+        paste0("\\s", unlist(strsplit(country, "\\|")), "$",   collapse = "|"),
+        paste0("\\s", unlist(strsplit(country, "\\|")), "\\s", collapse = "|")
+      )
+      
+      country <- paste0(country, collapse = "|")
+      
+      length(unlist(stringr::str_extract_all(page, country)))
+    })) 
+  })
+  
+  
+  ## Arrange object ----
+  
+  detections <- do.call(cbind.data.frame, detections)
+  colnames(detections) <- paste0("page", 1:ncol(detections))
+  
+  detections <- data.frame(
+    "geographic_entity" = world_countries$"geographic_entity",
+    detections)
+  
+  detections <- tidyr::pivot_longer(detections, cols = -1, 
+                                    names_to = "page", values_to = "count")
+  
+  detections <- as.data.frame(detections)
+  
+  detections$"page"   <- as.numeric(gsub("page", "", detections$"page"))
+  detections$"n_page" <- length(x)
+  
+
+  ## Keep only matches ----
+  
+  detections <- detections[detections$"count" > 0, ]
+  
+  detections <- detections[with(detections, order(page, count)), c(1, 4, 2, 3)]
+  
+  rownames(detections) <- NULL
+  
+  detections
+}
